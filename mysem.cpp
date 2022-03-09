9c8b909f9aef70464338a8c7982196bb08ff8b72
Zifeng Tang <tangzifeng37@yeah.net>
2022-03-09 18:28:43 +0000

    fix: cas

--- mysem.cpp
+++ mysem.cpp
@@ -3,6 +3,7 @@
 //
 #include "mysem.h"
 #include<thread>
+#include<linux/futex.h>
 using namespace std;
 
 
@@ -12,8 +13,22 @@ mysem::mysem(uint32_t init_value)
 }
 
 void mysem::acquire()
-{
-    this->counter.fetch_sub(1);
+{   
+    uint32_t expected;
+    
+    //loop while expected is smaller than 0
+    while(expected = counter.load(memory_order_seq_cst)<=0);
+    
+    //use cas to assign the value
+    while (!counter.compare_exchange_strong(expected, expected-1, memory_order_seq_cst))
+    {
+        if(expected<=0)
+        {
+            expected = 1;
+        }
+    }
+    
+    // this->counter.fetch_sub(1); 
 }
 
 void mysem::release()
@@ -24,23 +39,32 @@ void mysem::release()
 
 void random_work()
 {
-    cout<<"Damn you mother fucker!~~~~"<<endl;
+    for (volatile int i = 0; i < 10000; i++);
+    // cout<<"Some random works"<<endl;
 }
 
 
 int main(int argc, char**argv)
 {
-    mysem s(2);
-    std::thread t1([&](){
-    s.acquire();
-    std::cout << 1; random_work(); std::cout << 1;
-    s.release();
-    });
-    std::thread t2([&](){
-    s.acquire();
-    std::cout << 2; random_work(); std::cout << 2;
-    s.release();
-    });
-    t1.join(); t2.join();
-    std::cout << std::endl;
+    for(int i = 0; i < 100; i++){
+        mysem s(1);
+        std::thread t1([&](){
+        s.acquire();
+        std::cout << 1; random_work(); std::cout << 1;
+        s.release();
+        });
+        std::thread t2([&](){
+        s.acquire();
+        std::cout << 2; random_work(); std::cout << 2;
+        s.release();
+        });
+        std::thread t3([&](){
+        s.acquire();
+        std::cout << 3; random_work(); std::cout << 3;
+        s.release();
+        });
+        t1.join(); t2.join(); t3.join();
+        std::cout << std::endl;
+    }
+    
 }
